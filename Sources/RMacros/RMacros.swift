195ffe9c666523e9103fc2fcc5f68fc0a7115c53
Andrey Mosin <andreymosingit@gmail.com>
2024-09-13 09:48:01 +0200

    fix

--- Sources/RMacros/RMacros.swift
+++ Sources/RMacros/RMacros.swift
@@ -5,5 +5,5 @@ import ComposableArchitecture
 import SwiftUI
 
 @attached(member)
-public macro FeatureTest<R: Reducer, V: View>() = #externalMacro(module: "RMacrosMacros", type: "FeatureTestMacros")
+public macro FeatureTest<R: Reducer, V: View>() = #externalMacro(module: "RMacrosMacros", type: "FeatureTestMacro")
 
